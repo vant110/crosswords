b894903d2483515f3e0c9ef6adf1914e53e29496
Evgeniy Shamin <vant110@yandex.ru>
2022-12-04 13:43:03 +0400

    1-registration-and-authorization: Added the APIs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,14 @@
 using Crosswords.Db;
+using Crosswords.Db.Models;
+using Crosswords.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Npgsql;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
@@ -9,37 +17,150 @@ builder.Services
         .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
         .UseNpgsql(
             new NpgsqlConnectionStringBuilder(
-                builder.Configuration.GetConnectionString("DefaultConnection"))
-            { 
-                Password = builder.Configuration["DbPassword"]
+                builder.Configuration["Db:ConnectionString"])
+            {
+                IncludeErrorDetail = true,
+                Password = builder.Configuration["Db:Password"]
             }
-            .ConnectionString));
+            .ConnectionString))
+    .AddScoped<DbService>()
+    .AddScoped<PasswordHasher<Player>>()
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options => options
+            .TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                //ValidIssuer = "MyAuthServer",
+                ValidateAudience = false,
+                //ValidAudience = "MyAuthClient",
+                ValidateLifetime = false,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Convert.FromBase64String(
+                        builder.Configuration["JwtBearer:IssuerSigningKey"])),
+                ValidateIssuerSigningKey = true,
+            })
+        .Services
+    .AddAuthorization();
 var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 
+#region Пользователь
 
-if (app.Environment.IsDevelopment())
+app.MapPost("api/auth/signup", async (
+    HttpRequest request,
+    PasswordHasher<Player> passwordHasher,
+    DbService dbService) =>
 {
-    app.MapGet("/test", async (
-        CrosswordsContext db) =>
+    const string Message = "Логин занят";
+
+    try
     {
-        try
+        string login = request.Form["login"];
+        if (login == app.Configuration["Admin:Login"])
+            return Results.BadRequest(new { Message });
+
+        string password = request.Form["password"];
+        string passwordHash = passwordHasher.HashPassword(null, password);
+
+        int playerId = await dbService.InsertPlayerAsync(login, passwordHash);
+
+        var jwt = new JwtSecurityToken(
+            claims: new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, playerId.ToString(), ClaimValueTypes.Integer),
+                new Claim(ClaimTypes.Role, "player")
+            },
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Convert.FromBase64String(
+                        app.Configuration["JwtBearer:IssuerSigningKey"])),
+                SecurityAlgorithms.HmacSha256));
+
+        return Results.Json(new { bearerToken = new JwtSecurityTokenHandler().WriteToken(jwt) });
+    }
+    catch (DbUpdateException)
+    {
+        return Results.BadRequest(new { Message });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapPost("api/auth/signin", async (
+    HttpRequest request,
+    PasswordHasher<Player> passwordHasher,
+    DbService dbService) =>
+{
+    const string Message = "Логин или пароль неверен";
+
+    try
+    {
+        string login = request.Form["login"];
+        string password = request.Form["password"];
+        bool isAdmin;
+        var claims = new List<Claim>();
+
+        if (login == app.Configuration["Admin:Login"])
         {
-            return Results.Json(await db.Themes
-                .Select(t => t)
-                .ToListAsync());
+            if (password != app.Configuration["Admin:Password"])
+                return Results.BadRequest(new { Message });
+
+            isAdmin = true;
         }
-        catch (Exception ex)
+        else
         {
-            app.Logger.LogError(ex.ToString());
-            return Results.Problem(ex.ToString());
+            var player = await dbService.SelectPlayerAsync(login);
+
+            if (player == null
+                || passwordHasher.VerifyHashedPassword(
+                    null,
+                    player.PasswordHash,
+                    password)
+                == PasswordVerificationResult.Failed)
+                return Results.BadRequest(new { Message });
+
+            isAdmin = false;
+            claims.Add(new Claim(
+                ClaimTypes.NameIdentifier,
+                player.PlayerId.ToString(),
+                ClaimValueTypes.Integer));
         }
-    });
-}
+        claims.Add(new Claim(ClaimTypes.Role, isAdmin ? "admin" : "player"));
 
+        var jwt = new JwtSecurityToken(
+            claims: claims,
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Convert.FromBase64String(
+                        app.Configuration["JwtBearer:IssuerSigningKey"])),
+                SecurityAlgorithms.HmacSha256));
 
+        return Results.Json(new
+        {
+            isAdmin,
+            bearerToken = new JwtSecurityTokenHandler().WriteToken(jwt)
+        },
+        new System.Text.Json.JsonSerializerOptions
+        {
+            DefaultIgnoreCondition =
+                System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault
+        });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 
+#endregion
 
+app.MapGet("api/user", [Authorize] () => "User");
+app.MapGet("api/admin", [Authorize(Roles = "admin")] () => "Admin");
+app.MapGet("api/player", [Authorize(Roles = "player")] () => "Player");
 
 app.Run();
---
+++ backend/Services/DbService.cs
@@ -0,0 +1,42 @@
+﻿using Crosswords.Db;
+using Crosswords.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crosswords.Services
+{
+    public class DbService
+    {
+        private readonly ILogger<DbService> _logger;
+        private readonly CrosswordsContext _db;
+
+        public DbService(
+            ILogger<DbService> logger,
+            CrosswordsContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public async Task<int> InsertPlayerAsync(string login, string passwordHash)
+        {
+            var player = new Player
+            {
+                Login = login,
+                PasswordHash = passwordHash
+            };
+
+            _db.Players.Add(player);
+            await _db.SaveChangesAsync();
+
+            return player.PlayerId;
+        }
+
+        public async Task<Player?> SelectPlayerAsync(string login)
+        {
+            return await _db.Players
+                .Where(p => p.Login == login)
+                .SingleOrDefaultAsync();
+        }
+
+    }
+}
